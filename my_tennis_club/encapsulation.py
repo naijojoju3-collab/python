7e9284e44c0681150c02181332bc21494ff500be
naijo <naijojoju3@gmail.com>
2026-05-18 15:05:16 +0530

    encapsulation

--- my_tennis_club/encapsulation.py
+++ my_tennis_club/encapsulation.py
@@ -3,9 +3,9 @@ class person:
         self.name = name
         self.__age = age 
 
-        def get_age(self):
-            return self.__age
+    def get_age(self):
+        return self.__age
         
-        p1 = person("emil", 25)
-        print(p1.name)
-        print(p1.get_age())
+p1 = person("emil", 25)
+print(p1.name)
+print(p1.get_age())
--- my_tennis_club/poly.py
+++ my_tennis_club/poly.py
@@ -3,15 +3,15 @@ class car:
         self.brand = brand
         self.model = model
 
-        def move(self):
-            print("drive!")
+    def move(self):
+        print("drive!")
 
 class boat:
  def __init__(self, brand, model):
     self.brand = brand
     self.model = model
 
-def move(self):
+ def move(self):
    print("sail!")
 
 class plane:
@@ -19,10 +19,10 @@ class plane:
       self.brand = brand
       self.model = model
       
-      def move(self):
-         print("fly!")
+   def move(self):
+        print("fly!")
 
-car1 = =car("ford","mustang")
+car1  =car("ford","mustang")
 boat1 = boat("ibiza", "touring 20")
 plane1 = plane("boeing","747")
 
